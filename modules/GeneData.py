e4299e23adf4997be0c1e66794b2bfcbf0c92128
Andrea Pierleoni <andreap@ebi.ac.uk>
2016-06-03 10:35:00 +0100

    removed dependency on lineprofiler

--- modules/GeneData.py
+++ modules/GeneData.py
@@ -21,33 +21,33 @@ from settings import Config
 
 
 '''line profiler code'''
-# try:
-from line_profiler import LineProfiler
+try:
+    from line_profiler import LineProfiler
 
-def do_profile(follow=[]):
-    def inner(func):
-        def profiled_func(*args, **kwargs):
-            try:
-                profiler = LineProfiler()
-                profiler.add_function(func)
-                for f in follow:
-                    profiler.add_function(f)
-                profiler.enable_by_count()
+    def do_profile(follow=[]):
+        def inner(func):
+            def profiled_func(*args, **kwargs):
+                try:
+                    profiler = LineProfiler()
+                    profiler.add_function(func)
+                    for f in follow:
+                        profiler.add_function(f)
+                    profiler.enable_by_count()
+                    return func(*args, **kwargs)
+                finally:
+                    profiler.print_stats()
+                    print 'done'
+            return profiled_func
+        return inner
+
+except ImportError:
+    def do_profile(follow=[]):
+        "Helpful if you accidentally leave in production!"
+        def inner(func):
+            def nothing(*args, **kwargs):
                 return func(*args, **kwargs)
-            finally:
-                profiler.print_stats()
-                print 'done'
-        return profiled_func
-    return inner
-
-# except ImportError:
-#     def do_profile(follow=[]):
-#         "Helpful if you accidentally leave in production!"
-#         def inner(func):
-#             def nothing(*args, **kwargs):
-#                 return func(*args, **kwargs)
-#             return nothing
-#         return inner
+            return nothing
+        return inner
 '''end of line profiler code'''
 
 __author__ = 'andreap'
