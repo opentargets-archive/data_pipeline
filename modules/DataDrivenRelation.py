00205a6d8418a0cfbcb2db897f4fb61c3338248f
Andrea Pierleoni <andreap@ebi.ac.uk>
2016-06-11 09:18:14 +0100

    multiple storage workers do not improve performance

--- modules/DataDrivenRelation.py
+++ modules/DataDrivenRelation.py
@@ -204,7 +204,8 @@ class DataDrivenRelationProcess(object):
 
         storage_workers = [DistanceStorageWorker(queue_storage,
                                                  self.es,
-                                                 ) for i in range(multiprocessing.cpu_count())]
+                                                 # ) for i in range(multiprocessing.cpu_count())]
+                                                 ) for i in range(1)]
 
         for w in storage_workers:
             w.start()
