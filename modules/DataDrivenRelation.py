d277ae54b02f2d5b2219685a5ea5d5ec23ef6fa8
Andrea Pierleoni <andreap@ebi.ac.uk>
2016-06-14 15:53:06 +0100

    refined queue sizes

--- modules/DataDrivenRelation.py
+++ modules/DataDrivenRelation.py
@@ -23,11 +23,12 @@ from common.ElasticsearchLoader import Loader
 from common.ElasticsearchQuery import ESQuery
 from scipy.spatial.distance import pdist
 import time
+import math
 
-from common.Redis import RedisQueue, RedisQueueStatusReporter
+from common.Redis import RedisQueue, RedisQueueStatusReporter, RedisQueueWorkerProcess
 from settings import Config
 
-STORAGE_CHUNK_SIZE = 10000
+STORAGE_CHUNK_SIZE = 1000
 STORAGE_WORKERS = multiprocessing.cpu_count()/2
 
 class Relation(JSONSerializable):
@@ -142,11 +143,6 @@ class DistanceStorageWorker(Process):
                                                r2.to_json(),
                                                create_index=False)
                                     c += 1
-
-                                #TODO: store in es here
-                                Relation
-                                if c%1000==0:
-                                    logging.info('%i datapoint stored' % c)
                             except Exception, e:
                                 error = True
                                 logging.exception('Error processing key %s' % key)
@@ -154,6 +150,30 @@ class DistanceStorageWorker(Process):
                             self.queue_in.done(key, error=error, r_server=self.r_server)
 
                 logging.info('%s done processing' % self.name)
+
+
+class MatrixIteratorWorker(RedisQueueWorkerProcess):
+
+    def __init__(self,
+                 queue_in,
+                 redis_path,
+                 queue_out=None,
+                 matrix_data = None,
+                 key_list = None,
+                 ):
+        super(MatrixIteratorWorker, self).__init__(queue_in,redis_path, queue_out)
+        self.matrix_data = matrix_data
+        self.keys = key_list
+
+    def process(self, data):
+        i = data
+        for j in range(len(self.keys)):
+            if j >= i:
+                subj_id = self.keys[i]
+                obj_id = self.keys[j]
+                if set(self.matrix_data[subj_id].keys()) & set(self.matrix_data[obj_id].keys()):
+                    self.queue_out.put((subj_id, self.matrix_data[subj_id], obj_id, self.matrix_data[obj_id]),
+                                       r_server= self.r_server)
 
 
 
@@ -188,22 +208,35 @@ class DataDrivenRelationProcess(object):
 
         filtered_diseases = self.get_hot_node_blacklist(disease_data)
 
-
+        '''create the index'''
         Loader(self.es).create_new_index(Config.ELASTICSEARCH_RELATION_INDEX_NAME)
 
-        queue_processing = RedisQueue(queue_id=Config.UNIQUE_RUN_ID + '|ddr_processing',
-                           max_size=multiprocessing.cpu_count()*STORAGE_CHUNK_SIZE,
-                           job_timeout=20)
+        '''create the queues'''
+        d2d_queue_loading = RedisQueue(queue_id=Config.UNIQUE_RUN_ID + '|ddr_d2d_loading',
+                                       max_size=multiprocessing.cpu_count() * STORAGE_WORKERS*2,
+                                       job_timeout=20)
+        t2t_queue_loading = RedisQueue(queue_id=Config.UNIQUE_RUN_ID + '|ddr_t2t_loading',
+                                       max_size=multiprocessing.cpu_count() * STORAGE_CHUNK_SIZE * 2,
+                                       job_timeout=20)
 
-        q_reporter = RedisQueueStatusReporter([queue_processing])
-        q_reporter.start()
+        d2d_queue_processing = RedisQueue(queue_id=Config.UNIQUE_RUN_ID + '|ddr_d2d_processing',
+                                      max_size=multiprocessing.cpu_count()*STORAGE_CHUNK_SIZE,
+                                      job_timeout=20)
+        t2t_queue_processing = RedisQueue(queue_id=Config.UNIQUE_RUN_ID + '|ddr_t2t_processing',
+                                      max_size=multiprocessing.cpu_count() * STORAGE_CHUNK_SIZE,
+                                      job_timeout=20)
 
         queue_storage = RedisQueue(queue_id=Config.UNIQUE_RUN_ID + '|ddr_storage',
                                    max_size=int(STORAGE_CHUNK_SIZE*STORAGE_WORKERS*1.2),
                                    job_timeout=20)
-
-        q_reporter_storage = RedisQueueStatusReporter([queue_storage])
-        q_reporter_storage.start()
+        '''start shared workers'''
+        q_reporter = RedisQueueStatusReporter([d2d_queue_loading,
+                                               d2d_queue_processing,
+                                               t2t_queue_loading,
+                                               t2t_queue_processing,
+                                               queue_storage],
+                                              interval=60)
+        q_reporter.start()
 
         storage_workers = [DistanceStorageWorker(queue_storage,
                                                  self.es,
@@ -213,8 +246,8 @@ class DataDrivenRelationProcess(object):
         for w in storage_workers:
             w.start()
 
-        ''' compute disease to disease distances'''
-        d2d_workers = [DistanceComputationWorker(queue_processing,
+        '''start workers for d2d'''
+        d2d_workers = [DistanceComputationWorker(d2d_queue_processing,
                                                  [],
                                                  queue_storage,
                                                  RelationType.SHARED_TARGET,
@@ -222,164 +255,68 @@ class DataDrivenRelationProcess(object):
         for w in d2d_workers:
             w.start()
 
-        logging.info('Starting to compute disease to disease distances')
-        for pair in self.get_distance_pair(disease_data):
-            queue_processing.put(pair, self.r_server)
-        logging.info('disease to disease distances computation done')
+        disease_keys = disease_data.keys()
+        d2d_loader_workers = [MatrixIteratorWorker(d2d_queue_loading,
+                                                   self.r_server.db,
+                                                   d2d_queue_processing,
+                                                   disease_data,
+                                                   disease_keys,
+                                                   ) for i in range(multiprocessing.cpu_count())]
 
-        ''' compute target to target distances'''
-        t2t_workers = [DistanceComputationWorker(queue_processing,
+        for w in d2d_loader_workers:
+            w.start()
+
+        ''' compute disease to disease distances'''
+        logging.info('Starting to push pairs for disease to disease distances computation')
+        for i in range(len(disease_keys)):
+            d2d_queue_loading.put(i, self.r_server)
+        logging.info('disease to disease distances pair push done')
+
+        '''stop d2d specifc workers'''
+        d2d_queue_loading.submission_done(self.r_server)
+        for w in d2d_loader_workers:
+            w.join()
+        d2d_queue_processing.submission_done(self.r_server)
+        for w in d2d_workers:
+            w.join()
+
+        '''start workers for t2t'''
+        t2t_workers = [DistanceComputationWorker(t2t_queue_processing,
                                                  filtered_diseases,
                                                  queue_storage,
                                                  RelationType.SHARED_DISEASE,
                                                  ) for i in range(multiprocessing.cpu_count())]
         for w in t2t_workers:
             w.start()
+        target_keys = target_data.keys()
+        t2t_loader_workers = [MatrixIteratorWorker(t2t_queue_loading,
+                                                   self.r_server.db,
+                                                   t2t_queue_processing,
+                                                   target_data,
+                                                   target_keys,
+                                                   ) for i in range(multiprocessing.cpu_count())]
 
-        logging.info('Starting to compute disease to disease distances')
-        for pair in self.get_distance_pair(target_data):
-            queue_processing.put(pair, self.r_server)
-        logging.info('disease to disease distances computation done')
+        for w in t2t_loader_workers:
+            w.start()
 
+        ''' compute target to target distances'''
+        logging.info('Starting to push pairs for target to target distances computation')
+        for i in range(len(disease_keys)):
+            d2d_queue_loading.put(i, self.r_server)
+        logging.info('target to target distances pair push done')
 
-        queue_processing.submission_done(self.r_server)
+        '''stop d2d specifc workers'''
+        t2t_queue_loading.submission_done(self.r_server)
+        for w in t2t_loader_workers:
+            w.join()
+        t2t_queue_processing.submission_done(self.r_server)
+        for w in t2t_workers:
+            w.join()
 
-        while not queue_processing.is_done():
-            time.sleep(0.1)
+        '''stop storage workers'''
         queue_storage.submission_done(self.r_server)
-
-
-        # ''' compute disease to disease distances'''
-        # logging.info('Starting to compute disease to disease distances')
-        # start_time = time.time()
-        # disease_matrix = []
-        # f = itemgetter(*available_targets)
-        # c=0
-        # for d in available_diseases:
-        #     '''for every disease, get the default dict with data and transform it in a vector of float values keeping the same order of targets for all the diseases'''
-        #     disease_matrix.append(map(self.cap_to_one,f(disease_data[d])))
-        #     # print d, disease_data[d], disease_matrix[c-1][available_targets.index(disease_data[d].keys()[0])]
-        #     del disease_data[d]
-        #     c += 1
-        #     if c%1000==0:
-        #         print 'extracted', c
-        #
-        # disease_matrix = np.array(disease_matrix, copy=False)
-        # logging.info('Computed disease to disease matrix %i s' % (time.time() - start_time))
-        # logging.info('disease matrix size in memory: %f Kb' % (sys.getsizeof(disease_matrix) /1024.))
-        # del disease_data #possibly free up some memory
-        # logging.info('trying to remove disease data: %s' %(gc.collect()>1))
-        # d2d_c = 0
-        # vector_of_non_zero_target_counts = (disease_matrix != 0).sum(1) #array([0,1,10,0,10]) number of diseases sharing this target
-        # # print list(vector_of_non_zero_target_counts)
-        # # print (vector_of_non_zero_target_counts < 100)
-        # print 'filter by 10', (vector_of_non_zero_target_counts < 10).sum()
-        # print 'filter by 100', (vector_of_non_zero_target_counts < 100).sum()
-        # print 'filter by 1000', (vector_of_non_zero_target_counts < 1000).sum()
-        # print 'filter by 10000', (vector_of_non_zero_target_counts < 10000).sum()
-        #
-        # start_time = time.time()
-        # for i in range(len(available_diseases))[:30]:
-        #     for j in range(len(available_diseases)):
-        #         if j>=i:
-        #             subj = disease_matrix[i]
-        #             obj = disease_matrix[j]
-        #             if set(np.nonzero(subj)[0].tolist()) & set(np.nonzero(obj)[0].tolist()): # if they share a target
-        #                 d2d_c+=1
-        #                 dist = pdist([subj,obj])
-        #                 if i < 10 and j < 10 and i!= j:
-        #                     print dist
-        #                     if dist == 0:
-        #                         print subj == obj
-        #                         print np.nonzero(subj)[0].tolist()
-        #                         print np.nonzero(obj)[0].tolist()
-        #             else:
-        #                 # print 0.
-        #                 pass
-        #     print i+1, d2d_c, round(float(d2d_c)/(i+1)), round(float(d2d_c)/(time.time() - start_time))
-        #     # sys.exit(0)
-        #
-        # # disease_dists = pdist(disease_matrix)
-        # logging.info('Computed disease to disease distances in %i s' % (time.time() - start_time))
-        # # print disease_dists
-        # # logging.info('disease to disease distances matrix shape %s' % disease_dists.shape())
-        #
-        #
-        # ''' compute target to target distances'''
-        # logging.info('Starting to compute target to target distances')
-        # start_time = time.time()
-        # target_matrix = []
-        # f = itemgetter(*available_diseases)
-        # c = 0
-        # for d in available_targets:
-        #     c += 1
-        #     '''for every disease, get the default dict with data and transform it in a vector of float values keeping the same order of targets for all the diseases'''
-        #     target_matrix.append(np.array(map(self.cap_to_one, f(target_data[d]))))
-        #     del target_data[d]
-        #     if c % 1000 == 0:
-        #         print 'extracted', c
-        # logging.info('Computed target to disease matrix %i s' % (time.time() - start_time))
-        # logging.info('target matrix size in memory: %f Kb' % (sys.getsizeof(target_matrix) / 1024.))
-        # del target_data  # possibly free up some memory
-        # logging.info('trying to remove target data: %s' % (gc.collect() > 1))
-        # d2d_c = 0
-        # start_time = time.time()
-        # for i in range(len(available_targets))[:30]:
-        #     for j in range(len(available_targets)):
-        #         if j>=i:
-        #             subj = target_matrix[i]
-        #             obj = target_matrix[j]
-        #             if set(np.nonzero(subj)[0].tolist()) & set(np.nonzero(obj)[0].tolist()):  # if they share a target
-        #                 d2d_c += 1
-        #                 dist = pdist([subj,obj])
-        #                 if i<10 and j<10 and i != j:
-        #                     print dist
-        #                     if dist == 0:
-        #                         print subj == obj
-        #                         print np.nonzero(subj)[0].tolist()
-        #                         print np.nonzero(obj)[0].tolist()
-        #             else:
-        #                 # print 0.
-        #                 pass
-        #     print i + 1, d2d_c, round(float(d2d_c) / (i + 1)), round(float(d2d_c)/(time.time() - start_time))
-        #
-        # logging.info('Computed disease to disease distances in %i s' % (time.time() - start_time))
-
-        self.process_targets()
-        self.process_diseases()
-
-    def process_targets(self):
-        pass
-
-    def process_diseases(self):
-        pass
-
-    def get_distance_pair(self, data):
-        keys = data.keys()
-        space_size = (len(keys)**2)/2
-        total = 0
-        shared = 0
-        logging.info('Space to explore: %i combinations'%space_size)
-        for i in range(len(keys)):
-            for j in range(len(keys)):
-                if j>=i:
-                    total += 1
-                    subj_id = keys[i]
-                    obj_id = keys[j]
-                    if set(data[subj_id].keys()) & set(data[obj_id].keys()):
-                        shared +=1
-                        if shared % 10000 == 0:
-                            logging.info('explored %i pairs (%.2f%%), loaded %i for computation, rate: %.2f%%'%(total, float(total)/space_size*100, shared, float(shared)/total*100))
-                        yield subj_id, data[subj_id], obj_id, data[obj_id]
-
-    # def get_distance_pair_batches(self,data, batch_size=10):
-    #     batch = []
-    #     for i in self.get_distance_pair(data):
-    #         batch.append(i)
-    #         if len(batch) == batch_size:
-    #             yield batch
-    #             batch = []
-
+        for w in storage_workers:
+            w.start()
 
 
     def get_hot_node_blacklist(self, data):
@@ -389,3 +326,4 @@ class DataDrivenRelationProcess(object):
 
         logging.info('Most common diseases: %s'%c.most_common(10))
         return [i[0] for i in c.most_common(10)]
+
