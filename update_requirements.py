e9e5a1d14addff7b2ff28646ff936df55f55d481
Andrea Pierleoni <andreap@ebi.ac.uk>
2016-08-02 15:04:44 +0100

    added backbone for toolbox like usage

--- update_requirements.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'andreap'
-import pip
-with open("requirements.txt", "w") as f:
-    for dist in pip.get_installed_distributions():
-        req = dist.as_requirement()
-        f.write(str(req) + "\n")
