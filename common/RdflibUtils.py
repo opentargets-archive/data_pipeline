049b953f92d4dfa5db72d61f0f3316a59260fc15
wnewell <newell.wr@gmail.com>
2016-11-13 15:52:40 +0000

    Stub for RdflibUtils common methods - e.g get unique subjects and predicates.

---
+++ common/RdflibUtils.py
@@ -0,0 +1,19 @@
+__author__ = 'wnewell'
+"""
+Static methods for RDFLib Graphs
+"""
+
+
+def main():
+    print "main()"
+    test()
+
+
+def test():
+    print "test()"
+
+
+if __name__ == '__main__':
+    main()
+    
+    
