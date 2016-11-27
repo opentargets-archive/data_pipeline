c59ab3caa3856fb8e24ce2f9effcc46e5b5a44f6
Andrea Pierleoni <andreap@ebi.ac.uk>
2016-11-27 00:45:10 +0000

    bugfix

--- common/ElasticsearchQuery.py
+++ common/ElasticsearchQuery.py
@@ -43,7 +43,7 @@ class ESQuery(object):
 
     @staticmethod
     def _get_source_from_fields(fields = None):
-        if fields is None:
+        if fields:
             return True
         source = {"include": fields}
         return source
