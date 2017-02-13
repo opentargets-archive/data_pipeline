8f772160426d9545d92f49db82bda2be74d048b3
navygit <navyeinstein1991@gmail.com>
2017-02-13 12:09:39 +0000

    More refactoring

--- tests/test_GE.py
+++ tests/test_GE.py
@@ -7,24 +7,23 @@ ge_object = GE.GE()
 
 class MyTestCase(unittest.TestCase):
     def test_request_to_panel_app(self):
-        assert "Multiple endocrine tumours", "553f94b4bb5a1616e5ed4595" in ge_object.request_to_panel_app()
-        assert "Congenital myopathy", "553f94b6bb5a1616e5ed459a" in ge_object.request_to_panel_app()
-        assert "Renal tubular acidosis", "553f94d5bb5a1616e5ed45a4" in ge_object.request_to_panel_app()
+        self.assertIn("Multiple endocrine tumours", "553f94b4bb5a1616e5ed4595", ge_object.request_to_panel_app())
+        self.assertIn("Congenital myopathy", "553f94b6bb5a1616e5ed459a", ge_object.request_to_panel_app())
+        self.assertIn("Renal tubular acidosis", "553f94d5bb5a1616e5ed45a4", ge_object.request_to_panel_app())
 
     def test_execute_ge_request(self):
-        assert 'Meningioma' in ge_object.execute_ge_request()
-        assert 'ischemic stroke' in ge_object.execute_ge_request()
-        assert 'Perlman Syndrome' in ge_object.execute_ge_request()
+        self.assertIn('Meningioma',ge_object.execute_ge_request())
+        self.assertIn('ischemic stroke', ge_object.execute_ge_request())
+        self.assertIn('Perlman Syndrome', ge_object.execute_ge_request())
 
     def test_request_to_zooma(self):
-        assert 'Meningioma' in ge_object.request_to_zooma('Meningioma')
-        assert 'http://www.orpha.net/ORDO/Orphanet_2495' in ge_object.request_to_zooma('Meningioma')['Meningioma'][
-            'uri']
+        self.assertIn('Meningioma', ge_object.request_to_zooma('Meningioma'))
+        self.assertIn('http://www.orpha.net/ORDO/Orphanet_2495', ge_object.request_to_zooma('Meningioma')['Meningioma']['uri'])
 
     def test_evidence_strings(self):
         with open(Config.GE_EVIDENCE_STRING, 'r') as evidence_file:
             for row in evidence_file:
-                assert "validated_against_schema_version" in row
+                self.assertIn("validated_against_schema_version", row)
 
 
 if __name__ == '__main__':
