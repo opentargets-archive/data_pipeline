c5d44917dca917979dc3dd0d4f31f39b9ddf620c
navygit <navyeinstein1991@gmail.com>
2017-02-13 11:56:48 +0000

    added unit test module and refactored the tests

--- tests/test_GE.py
+++ tests/test_GE.py
@@ -1,31 +1,34 @@
+import unittest
 from modules import GE
 from settings import Config
 
 ge_object = GE.GE()
 
 
-def test_request_to_panel_app():
-    assert "Multiple endocrine tumours", "553f94b4bb5a1616e5ed4595" in ge_object.request_to_panel_app()
-    assert "Congenital myopathy", "553f94b6bb5a1616e5ed459a" in ge_object.request_to_panel_app()
-    assert "Renal tubular acidosis", "553f94d5bb5a1616e5ed45a4" in ge_object.request_to_panel_app()
+class MyTestCase(unittest.TestCase):
+    def test_request_to_panel_app(self):
+        assert "Multiple endocrine tumours", "553f94b4bb5a1616e5ed4595" in ge_object.request_to_panel_app()
+        assert "Congenital myopathy", "553f94b6bb5a1616e5ed459a" in ge_object.request_to_panel_app()
+        assert "Renal tubular acidosis", "553f94d5bb5a1616e5ed45a4" in ge_object.request_to_panel_app()
+
+    def test_execute_ge_request(self):
+        assert 'Meningioma' in ge_object.execute_ge_request()
+        assert 'ischemic stroke' in ge_object.execute_ge_request()
+        assert 'Perlman Syndrome' in ge_object.execute_ge_request()
+
+    def test_request_to_zooma(self):
+        assert 'Meningioma' in ge_object.request_to_zooma('Meningioma')
+        assert 'http://www.orpha.net/ORDO/Orphanet_2495' in ge_object.request_to_zooma('Meningioma')['Meningioma'][
+            'uri']
+
+    def test_evidence_strings(self):
+        with open(Config.GE_EVIDENCE_STRING, 'r') as evidence_file:
+            for row in evidence_file:
+                assert "validated_against_schema_version" in row
 
 
-def test_execute_ge_request():
-    assert 'Meningioma' in ge_object.execute_ge_request()
-    assert 'ischemic stroke' in ge_object.execute_ge_request()
-    assert 'Perlman Syndrome' in ge_object.execute_ge_request()
-
-
-def test_request_to_zooma():
-    assert 'Meningioma' in ge_object.request_to_zooma('Meningioma')
-    assert 'http://www.orpha.net/ORDO/Orphanet_2495' in ge_object.request_to_zooma('Meningioma')['Meningioma']['uri']
-
-
-def test_evidence_strings():
-    with open(Config.GE_EVIDENCE_STRING, 'r') as evidence_file:
-        for row in evidence_file:
-            assert "validated_against_schema_version" in row
-
+if __name__ == '__main__':
+    unittest.main()
 
 
 
