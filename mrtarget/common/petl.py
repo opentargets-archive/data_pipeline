62a66b3b89c9493985a9d1085b05cbafddded847
Miguel C <mkarmona@gmail.com>
2017-05-09 11:39:55 +0100

    fix: proper way to get a url and put into a os-portable tempfile
    refactor into a function and put it in .io package and .petl package. Also,
    refactored a couple of lines trying to meet with a future python 3.x interfaces
    
    Reference #106

--- mrtarget/common/petl.py
+++ mrtarget/common/petl.py
@@ -1,57 +1,69 @@
-import gzip
-import urllib2
-from io import BytesIO
-import zipfile
-import requests as r
+from __future__ import absolute_import
 
 from contextlib import contextmanager
-from petl.errors import ArgumentError
+import gzip
+import zipfile
+import petl as p
 
-from mrtarget.Settings import Config
+from mrtarget.common.io import url_to_tmpfile
 
 
 class URLZSource():
     def __init__(self, *args, **kwargs):
+        '''A source extension for petl python package
+        Just in case you need to use proxies for url use it as normal
+        named arguments
+
+        >>> # proxies = {}
+        >>> # if Config.HAS_PROXY:
+        ...    # self.proxies = {"http": Config.PROXY,
+        ...                      # "https": Config.PROXY}
+        >>> # with URLZSource('http://var.foo/noname.csv',proxies=proxies) as f:
+        >>> from __futures__ import absolute_import
+        >>> import petl as p
+        >>> t = p.fromcsv(mpetl.URLZSource('https://raw.githubusercontent.com/opentargets/mappings/master/expression_uberon_mapping.csv'), delimiter='|')
+        >>> t.look()
+
+        +------------------------+-------------------+
+        | label                  | uberon_code       |
+        +========================+===================+
+        | u'stomach 2'           | u'UBERON_0000945' |
+        +------------------------+-------------------+
+        | u'stomach 1'           | u'UBERON_0000945' |
+        +------------------------+-------------------+
+        | u'mucosa of esophagus' | u'UBERON_0002469' |
+        +------------------------+-------------------+
+        | u'transverse colon'    | u'UBERON_0001157' |
+        +------------------------+-------------------+
+        | u'brain'               | u'UBERON_0000955' |
+        +------------------------+-------------------+
+        ...
+
+        '''
         self.args = args
         self.kwargs = kwargs
         self.proxies = None
-        if Config.HAS_PROXY:
-            self.proxies = {"http": Config.PROXY,
-                            "https": Config.PROXY}
 
     @contextmanager
     def open(self, mode='r'):
         if not mode.startswith('r'):
-            raise ArgumentError('source is read-only')
+            raise p.errors.ArgumentError('source is read-only')
 
-        zf = None
-        f = None
+        with url_to_tmpfile(*self.args, **self.kwargs) as f:
+            buf = f
+            zf = None
 
-        if self.proxies:
-            f = r.get(*self.args,
-                      proxies=self.proxies,
-                      stream=True, **self.kwargs)
-        else:
-            f = r.get(*self.args,
-                      stream=True, **self.kwargs)
+            if self.args[0].endswith('.gz'):
+                zf = gzip.GzipFile(fileobj=buf)
+            elif self.args[0].endswith('.zip'):
+                zipped_data = zipfile.ZipFile(buf)
+                info = zipped_data.getinfo(
+                    zipped_data.filelist[0].orig_filename)
+                zf = zipped_data.open(info)
+            else:
+                zf = buf
 
-        f.raise_for_status()
-
-        request = urllib2.Request(*self.args, **self.kwargs)
-        f = urllib2.urlopen(request)
-        buf = BytesIO(f.read())
-
-        if self.args[0].endswith('.gz'):
-            zf = gzip.GzipFile(fileobj=buf)
-        elif self.args[0].endswith('.zip'):
-            zipped_data = zipfile.ZipFile(buf)
-            info = zipped_data.getinfo(zipped_data.filelist[0].orig_filename)
-            zf = zipped_data.open(info)
-        else:
-            zf = buf
-
-        try:
-            yield zf
-        finally:
-            zf.close()
-            f.close()
+            try:
+                yield zf
+            finally:
+                zf.close()
