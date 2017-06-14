be393b2cbb3691e964e77485f2369d1893fc4337
Miguel C. <mkarmona@gmail.com>
2017-06-14 16:13:48 +0100

    fix: miss to commit regex string changes

--- mrtarget/Settings.py
+++ mrtarget/Settings.py
@@ -258,8 +258,8 @@ class Config():
     EVIDENCEVALIDATION_PROVIDER_EMAILS["cttv011"] = [ 'eddturner@ebi.ac.uk', 'bpalka@ebi.ac.uk' ]
     EVIDENCEVALIDATION_PROVIDER_EMAILS["cttv012"] = [ 'tsmith@ebi.ac.uk', 'garys@ebi.ac.uk', 'cyenyxe@ebi.ac.uk' ]
     EVIDENCEVALIDATION_PROVIDER_EMAILS["cttv025"] = [ 'kafkas@ebi.ac.uk', 'ftalo@ebi.ac.uk' ]
-    EVIDENCEVALIDATION_FILENAME_REGEX = re.compile(r""".*
-    (?P<datasource>[a-zA-Z0-9_]*).*(\-
+    EVIDENCEVALIDATION_FILENAME_REGEX = re.compile(r"""
+    (?P<datasource>[a-zA-Z0-9_]+)(\-
     (?P<d1>\d{2})\-
     (?P<d2>\d{2})\-
     (?P<d3>\d{4}))?
