866e53471ef7fd8605db4cf80c3af59d6c44dfba
Andrea Pierleoni <andreap@ebi.ac.uk>
2017-07-11 15:49:47 +0100

    strip prefix for data version injection in json objects

--- mrtarget/common/DataStructure.py
+++ mrtarget/common/DataStructure.py
@@ -32,14 +32,14 @@ def json_serialize(obj):
 class JSONAddict(Dict):
     def load_json(self, data):
         self.update(json.loads(data))
-    
+
     def to_json(self):
         return json.dumps(self.__timestamp(self).to_dict(),
                           default=json_serialize,
                           sort_keys=True,
                           # indent=4,
                           cls=PipelineEncoder)
-    
+
     @staticmethod
     def __timestamp(data):
         '''this method add the data_release key to the addict.Dict'''
@@ -65,7 +65,7 @@ class JSONSerializable(object):
             raise AttributeError("cannot load JSON object from %s type"%str(type(data)))
 
     def stamp_data_release(self):
-        self.__dict__['data_release'] = Config.RELEASE_VERSION
+        self.__dict__['data_release'] = Config.RELEASE_VERSION.split('-')[-1]
 
 
 class TreeNode(object):
