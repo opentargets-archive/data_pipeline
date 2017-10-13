10efbec0c0b872e4d041353bf7e620583de4a5f0
Miguel C. <mkarmona@gmail.com>
2017-10-13 14:59:31 +0100

    fix: TqdmToLogger missing from lookup table and breaking everything

--- mrtarget/common/LookupTables.py
+++ mrtarget/common/LookupTables.py
@@ -4,6 +4,7 @@ from mrtarget.common.ElasticsearchQuery import ESQuery
 from mrtarget.common.Redis import RedisLookupTablePickle
 from mrtarget.common.connection import new_redis_client, PipelineConnectors
 from mrtarget.Settings import Config
+from mrtarget.common import TqdmToLogger
 
 class HPALookUpTable(object):
     """
