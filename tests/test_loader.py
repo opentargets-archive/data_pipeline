f517a0a0cdeb2e43c5f3037909c8e92baf1694ce
Andrea Pierleoni <andreap@ebi.ac.uk>
2017-10-17 16:26:05 +0100

    adds test for loader and redis lookup tables

---
+++ tests/test_loader.py
@@ -0,0 +1,30 @@
+import unittest
+from mrtarget.common.ElasticsearchLoader import Loader
+
+
+class ElasticsearchLoaderTestCase(unittest.TestCase):
+
+    def test_put(self):
+        loader = Loader(dry_run=True)
+        loader.put('dummy-index',
+                   'dummy-doctype',
+                   'id',
+                   '{"hello":"test"}')
+        self.assertEquals(len(loader.cache),1)
+        loader.flush()
+        self.assertEquals(len(loader.cache),0)
+
+    def test_many_put(self):
+        loader = Loader(dry_run=True,
+                        chunk_size=100)
+        for i in range(150):
+            loader.put('dummy-index',
+                       'dummy-doctype',
+                       'id',
+                       '{"hello":"test"}')
+        self.assertEquals(len(loader.cache),50)
+        loader.close()
+        self.assertEquals(len(loader.cache),0)
+
+
+
