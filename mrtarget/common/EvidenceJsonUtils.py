3bc4521beec7f6385481bbae76d1b79294d6ae48
Miguel C <mkarmona@gmail.com>
2018-04-16 18:40:40 +0100

    fix: properly print the difflines to a warning log
    Reference #

--- mrtarget/common/EvidenceJsonUtils.py
+++ mrtarget/common/EvidenceJsonUtils.py
@@ -24,10 +24,10 @@ def assertJSONEqual(a, b, msg='Values are not equal', keys = []):
     result = list(
         d.compare(a.splitlines(1),
                   b.splitlines(1)))
-    for line in result:
-        if line[0] != ' ':
-            sys.stderr.writelines(result)
-            logger.warning("there is a difference between the previous index mapping and the new one", msg)
+
+    diffLines = [line for line in result if line[0] != ' ']
+    logger.warning("there is a difference between the previous index mapping and the new one \n%s", "\n".join(diffLines))
+
     return True
 
 class DatatStructureFlattener:
