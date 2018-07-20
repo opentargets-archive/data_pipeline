0eefed5c2f3da68ba4633100ad52dff33e26e38c
ckong <ckong@ebi.ac.uk>
2018-07-20 21:49:49 +0100

    move around structure to first get all molecules then add other data

--- mrtarget/modules/Compound.py
+++ mrtarget/modules/Compound.py
@@ -26,42 +26,51 @@ class CompoundProcess():
         self._logger = logging.getLogger(__name__)
         self.loader = loader
 
-        self.CHEMBL_COMPOUND = 'https://www.ebi.ac.uk/chembl/api/data/molecule.json'
-        self.cpd = {}
+        self.cpd_ids = []
+        self.cpd_data = {}
 
     def process(self):
-        self.parse_compound_data()
+
+        self.get_cpd_ids()
+        self.parse_cpd_data()
         self.store_to_elasticsearch()
 
-    def parse_compound_data(self):
-        ct = 0
-        compounds = get_chembl_data(self.CHEMBL_COMPOUND)
+    def get_cpd_ids(self):
 
-        for i in tqdm(compounds,
-                      desc='Downloading compound/drug attributes, indications & mechanisms from ChEMBL API',
-                      unit=' compound'):
+        ct = 0
+        data = self.query_rest_api(Config.CHEMBL_MOLECULE)
+
+        for i in tqdm(data,
+                  desc='Extract compound IDs from ChEMBL API',
+                  unit=' compound(s)'):
+
+            if ct == 1000:
+                break
 
             cpd_id = i['molecule_chembl_id']
-            cpd_attrib = i
+
+            if cpd_id not in self.cpd_ids:
+                self.cpd_ids.append(cpd_id)
+                ct += 1
+
+    def parse_cpd_data(self):
+
+        for cpd_id in tqdm(self.cpd_ids,
+                  desc='Extract compound attributes, indications & mechanisms from ChEMBL API',
+                  unit=' compound(s)'):
+
+            cpd_attributes = self.download_from_uri(cpd_id, Config.CHEMBL_MOLECULE)
             cpd_indication = self.download_from_uri(cpd_id, Config.CHEMBL_INDICATION)
             cpd_mechanism = self.download_from_uri(cpd_id, Config.CHEMBL_MECHANISM)
 
-            if cpd_indication is None and cpd_mechanism is None:
-                break
-
-            #if ct == 10000:
-            #    break
-            if cpd_id not in self.cpd:
-                self.cpd[cpd_id] = \
+            if cpd_id not in self.cpd_data:
+                self.cpd_data[cpd_id] = \
                     {
-                        'attributes': cpd_attrib,
+                        'attributes': cpd_attributes['molecules'],
                         'indications': [],
-                        'mechanism': []
+                        'mechanisms': []
                     }
 
-            '''
-            Parse compound/drug-disease indications
-            '''
             if cpd_indication:
                 for row in cpd_indication['drug_indications']:
 
@@ -76,70 +85,69 @@ class CompoundProcess():
                         }
 
                     try:
-                        self.cpd[cpd_id]['indications'].append(line)
+                        self.cpd_data[cpd_id]['indications'].append(line)
                     except KeyError:
-                        self.cpd[cpd_id]['indications'] = list()
-                        self.cpd[cpd_id]['indications'].append(line)
+                        self.cpd_data[cpd_id]['indications'] = list()
+                        self.cpd_data[cpd_id]['indications'].append(line)
 
-            '''
-            Parse compound/drug-gene mechanisms of action
-            '''
             if cpd_mechanism:
                 for row in cpd_mechanism['mechanisms']:
 
                     try:
-                        self.cpd[cpd_id]['mechanisms'].append(row)
+                        self.cpd_data[cpd_id]['mechanisms'].append(row)
                     except KeyError:
-                        self.cpd[cpd_id]['mechanisms'] = list()
-                        self.cpd[cpd_id]['mechanisms'].append(row)
-            ct += 1
+                        self.cpd_data[cpd_id]['mechanisms'] = list()
+                        self.cpd_data[cpd_id]['mechanisms'].append(row)
+
+    def query_rest_api(self, uri):
+        '''return to json from uri'''
+        next_get = True
+        limit = 1000000
+        offset = 0
+
+        def _fmt(**kwargs):
+            '''generate uri string params from kwargs dict'''
+            l = ['='.join([k, str(v)]) for k, v in kwargs.iteritems()]
+            return '?' + '&'.join(l)
+
+        while next_get:
+            chunk = None
+            with c.URLZSource(uri + _fmt(limit=limit, offset=offset)).open() as f:
+                chunk = json.loads(f.read())
+
+            page_meta = chunk['page_meta']
+            data_key = list(set(chunk.keys()) - set(['page_meta']))[0]
+
+            if 'next' in page_meta and page_meta['next'] is not None:
+                limit = page_meta['limit']
+                offset += limit
+            else:
+                next_get = False
+
+            for el in chunk[data_key]:
+                yield el
 
     def download_from_uri(self, compound_id, uri):
-        url = uri + '?molecule_chembl_id=' + compound_id
+            url = uri + '?molecule_chembl_id=' + compound_id
 
-        try:
-            r = requests.get(url, timeout=300)
-            if r.status_code == 200:
-                return r.json()
-            else:
-                raise IOError('failed to get data from ChEMBL API')
+            try:
+                r = requests.get(url, timeout=300)
+                if r.status_code == 200:
+                    return r.json()
+                else:
+                    raise IOError('failed to get data from ChEMBL API')
 
-        except (ConnectionError, Timeout, HTTPError) as e:
-            raise IOError(e)
+            except (ConnectionError, Timeout, HTTPError) as e:
+                raise IOError(e)
 
     def store_to_elasticsearch(self):
 
-        for key, data in self.cpd.items():
+        self._logger.debug('Store compound data into ElasticSearch')
+
+        for key, data in self.cpd_data.items():
             self.loader.put(Config.ELASTICSEARCH_COMPOUND_INDEX_NAME,
                             Config.ELASTICSEARCH_COMPOUND_DOC_NAME,
                             key,
                             json.dumps(data),
                             True)
 
-def get_chembl_data(uri):
-    '''return to json from uri'''
-    next_get = True
-    limit = 1000000
-    offset = 0
-
-    def _fmt(**kwargs):
-        '''generate uri string params from kwargs dict'''
-        l = ['='.join([k, str(v)]) for k, v in kwargs.iteritems()]
-        return '?' + '&'.join(l)
-
-    while next_get:
-        chunk = None
-        with c.URLZSource(uri + _fmt(limit=limit, offset=offset)).open() as f:
-            chunk = json.loads(f.read())
-
-        page_meta = chunk['page_meta']
-        data_key = list(set(chunk.keys()) - set(['page_meta']))[0]
-
-        if 'next' in page_meta and page_meta['next'] is not None:
-            limit = page_meta['limit']
-            offset += limit
-        else:
-            next_get = False
-
-        for el in chunk[data_key]:
-            yield el
