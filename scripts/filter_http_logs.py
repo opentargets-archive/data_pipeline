e2e8b2993067caa8b326b2bf1418aecfd082a98b
Docproc <glenn@docproc.com>
2018-10-11 14:56:18 +0100

    Minor PEP8 style fixes

--- scripts/filter_http_logs.py
+++ scripts/filter_http_logs.py
@@ -15,16 +15,18 @@ urls = []
 
 for filename in sys.argv[1:]:
 
-    with open(filename) as file:
+    with open(filename) as log_file:
 
-        for line in file:
+        for line in log_file:
             line = line.rstrip()
 
             # Skip Elasticsearch calls - only works if ES is running on default port
-            if ':9200' in line: continue
+            if ':9200' in line:
+                continue
 
             # Skip 'Starting new'
-            if line.startswith('Starting new'): continue
+            if line.startswith('Starting new'):
+                continue
 
             # Line will now be of the form
             # http://rest.ensembl.org:80 "POST /lookup/id HTTP/1.1" 200 35864
@@ -32,18 +34,19 @@ for filename in sys.argv[1:]:
             # Strip "
             line = line.replace('"', '')
             parts = line.split()
-            if len(parts) != 6: continue
+            if len(parts) != 6:
+                continue
 
             (base, verb, path, protocol, return_code, size) = parts
             url_string = base + path
 
-            if (STRIP_QUERY):
+            if STRIP_QUERY:
                 url_parts = urlparse(url_string)
                 url_string = url_parts.scheme + '://' + url_parts.netloc + url_parts.path
 
             urls.append(url_string)
 
-# Uniquify list
+# Output unique list
 unique_urls = set(urls)
 for url in unique_urls:
     print url
