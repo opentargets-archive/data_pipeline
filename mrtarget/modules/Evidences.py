7372259aba6910f1d2cb7de1dea49972b71d912b
Miguel C <mkarmona@gmail.com>
2018-11-13 14:24:49 +0000

    wip: val step reimplemented
    Reference #rewrite

--- mrtarget/modules/Evidences.py
+++ mrtarget/modules/Evidences.py
@@ -8,6 +8,7 @@ import pypeln.process as pr
 import addict
 import uuid
 import codecs
+import functools
 import itertools
 import more_itertools
 
@@ -23,7 +24,10 @@ from mrtarget.common.LookupHelpers import LookUpDataRetriever, LookUpDataType
 from mrtarget.common.connection import new_redis_client, new_es_client, PipelineConnectors
 
 
-def validate_evidence_on_start():
+def validate_evidence_on_start(luts=None):
+    '''this function is called once per started process and return a
+    ProcessContext per process.
+    '''
     pc = ProcessContext()
     pc.logger.debug("called validate_evidence on_start from %s", str(os.getpid()))
     pc.logger.debug("creating schema validators")
@@ -34,14 +38,26 @@ def validate_evidence_on_start():
         pc.logger.info('generate_validator_from_schema %s using the uri %s', schema_name, schema_uri)
         pc.kwargs.validators[schema_name] = \
             opentargets_validator.helpers.generate_validator_from_schema(schema_uri)
+        pc.kwargs.luts = luts
+        pc.kwargs.redis_c = new_redis_client()
+        pc.kwargs.luts.set_r_server(pc.kwargs.redis_c)
     return pc
 
 
 def validate_evidence_on_done(_status, process_context):
+    '''this is called once when the process is finished computing. It is
+    potentially useful for closing sockets or fds'''
     process_context.logger.debug("called validate_evidence on_done from %s", str(os.getpid()))
 
 
 def validate_evidence(line, process_context):
+    '''this function is called once per line until number of lines is exhausted. process_context
+    is the object built and returned by its start function
+
+    It returns a tuple with (left, right) where left is the faulty line and the
+    right is the fully validated and processed. There is a specific case where you
+    get (None, None) which means we are not quetting the right expected input
+    '''
     if not line or line is None or len(line) != 2:
         process_context.logger.error('line != triple and this is weird as if any line you must have a triple')
         return (None, None)
@@ -53,6 +69,7 @@ def validate_evidence(line, process_context):
 
     try:
         data_type = None
+        data_source = None
         parsed_line = None
         parsed_line_bad = None
 
@@ -81,6 +98,18 @@ def validate_evidence(line, process_context):
 
         if data_type not in Config.EVIDENCEVALIDATION_DATATYPES:
             validated_evs.explanation_type = 'unsupported_datatype'
+            validated_evs.explanation_str = data_type
+            return (validated_evs, None)
+
+        if ('sourceID' not in parsed_line):
+            validated_evs.explanation_type = 'missing_datasource'
+            return (validated_evs, None)
+
+        data_source = parsed_line['sourceID']
+
+        if data_source not in Config.DATASOURCE_TO_DATATYPE_MAPPING:
+            validated_evs.explanation_type = 'unsupported_datasource'
+            validated_evs.explanation_str = data_source
             return (validated_evs, None)
 
         # validate line
@@ -106,7 +135,7 @@ def validate_evidence(line, process_context):
         efo_id = None
         # generate fantabulous dict from addict
         evidence_obj = addict.Dict(parsed_line)
-        evidence_obj.unique_association_fields['datasource'] = evidence_obj.sourceID
+        evidence_obj.unique_association_fields['datasource'] = data_source
 
         if evidence_obj.target.id:
             target_id = evidence_obj.target.id
@@ -117,6 +146,92 @@ def validate_evidence(line, process_context):
         validated_evs.hash = \
             DatatStructureFlattener(evidence_obj.unique_association_fields).get_hexdigest()
         evidence_obj.id = validated_evs.hash
+
+        disease_failed = False
+        target_failed = False
+
+        if efo_id:
+            # Check disease term or phenotype term
+            # if (short_disease_id not in self.lookup_data.available_efos) and \
+            if (efo_id not in process_context.kwargs.luts.lookup_data.efo_ontology.current_classes) and \
+                    (efo_id not in process_context.kwargs.luts.lookup_data.hpo_ontology.current_classes) and \
+                    (efo_id not in process_context.kwargs.luts.lookup_data.mp_ontology.current_classes):
+                validated_evs.explanation_type = 'invalid_disease'
+                validated_evs.explanation_str = efo_id
+                disease_failed = True
+            if (efo_id in process_context.kwargs.luts.lookup_data.efo_ontology.obsolete_classes) or \
+                    (efo_id in process_context.kwargs.luts.lookup_data.hpo_ontology.obsolete_classes) or \
+                    (efo_id in process_context.kwargs.luts.lookup_data.mp_ontology.obsolete_classes):
+                validated_evs.explanation_type = 'obsolete_disease'
+                validated_evs.explanation_str = efo_id
+                disease_failed = True
+        else:
+            validated_evs.explanation_type = 'missing_disease'
+            disease_failed = True
+
+        # CHECK GENE/PROTEIN IDENTIFIER Check Ensembl ID, UniProt ID
+        # and UniProt ID mapping to a Gene ID
+        # http://identifiers.org/ensembl/ENSG00000178573
+        if target_id:
+            if 'ensembl' in target_id:
+                ensembl_id = target_id.split('/')[-1]
+                if not ensembl_id in process_context.kwargs.luts.lookup_data.available_genes:
+                    validated_evs.explanation_type = 'invalid_target'
+                    validated_evs.explanation_str = ensembl_id
+                    target_failed = True
+
+                elif ensembl_id in process_context.kwargs.luts.lookup_data.non_reference_genes:
+                    process_context.logger.warning('nonref ensembl gene found %s line_n %d filename %s',
+                                                   ensembl_id, line_n, filename)
+
+            elif 'uniprot' in target_id:
+                uniprot_id = target_id.split('/')[-1]
+                if uniprot_id not in process_context.kwargs.luts.lookup_data.uni2ens:
+                    validated_evs.explanation_type = 'unknown_uniprot_entry'
+                    validated_evs.explanation_str = uniprot_id
+                    target_failed = True
+
+                elif not process_context.kwargs.luts.lookup_data.uni2ens[uniprot_id]:
+                    validated_evs.explanation_type = 'missing_ensembl_xref_for_uniprot_entry'
+                    validated_evs.explanation_str = uniprot_id
+                    target_failed = True
+
+                elif (uniprot_id in process_context.kwargs.luts.lookup_data.uni2ens) and \
+                        process_context.kwargs.luts.lookup_data.uni2ens[uniprot_id] in process_context.kwargs.luts.lookup_data.available_genes and \
+                        'is_reference' in process_context.kwargs.luts.lookup_data.available_genes[process_context.kwargs.luts.lookup_data.uni2ens[uniprot_id]] and \
+                        (not process_context.kwargs.luts.lookup_data.available_genes[process_context.kwargs.luts.lookup_data.uni2ens[uniprot_id]]['is_reference'] is True):
+                    validated_evs.explanation_type = 'nonref_ensembl_xref_for_uniprot_entry'
+                    validated_evs.explanation_str = uniprot_id
+                    target_failed = True
+                else:
+                    try:
+                        reference_target_list = \
+                            process_context.kwargs.luts.lookup_data.available_genes[process_context.kwargs.luts.lookup_data.uni2ens[uniprot_id]]['is_reference'] is True
+                    except KeyError:
+                        reference_target_list = []
+
+                    if reference_target_list:
+                        target_id = 'http://identifiers.org/ensembl/%s' % reference_target_list[0]
+                    else:
+                        target_id = process_context.kwargs.luts.lookup_data.uni2ens[uniprot_id]
+                    if target_id is None:
+                        validated_evs.explanation_type = 'missing_target_id_for_protein'
+                        validated_evs.explanation_str = uniprot_id
+                        target_failed = True
+
+        # If there is no target id after the processing step
+        if target_id is None:
+            validated_evs.explanation_type = 'missing_target_id'
+            target_failed = True
+
+        if target_failed or disease_failed:
+
+            if target_failed and disease_failed:
+                validated_evs.explanation_type = 'target_id_and_disease_id'
+                validated_evs.explanation_str = ''
+
+            return (validated_evs, None)
+
         return (None, evidence_obj)
 
     except Exception as e:
@@ -125,88 +240,10 @@ def validate_evidence(line, process_context):
         return (validated_evs, None)
 
 
-    return (None, None)
-
-    if efo_id:
-        # Check disease term or phenotype term
-        #if (short_disease_id not in self.lookup_data.available_efos) and \
-        if (efo_id not in self.lookup_data.efo_ontology.current_classes) and \
-                (efo_id not in self.lookup_data.hpo_ontology.current_classes) and \
-                (efo_id not in self.lookup_data.mp_ontology.current_classes):# or \
-                # (disease_id in self.efo_uncat):
-            explanation['invalid_disease'] = efo_id
-            disease_failed = True
-        if (efo_id in self.lookup_data.efo_ontology.obsolete_classes) or \
-                (efo_id in self.lookup_data.hpo_ontology.obsolete_classes) or \
-                (efo_id in self.lookup_data.mp_ontology.obsolete_classes):
-
-            explanation['obsolete_disease'] = efo_id
-    else:
-        explanation['missing_disease'] = True
-        disease_failed = True
-
-    # CHECK GENE/PROTEIN IDENTIFIER Check Ensembl ID, UniProt ID
-    # and UniProt ID mapping to a Gene ID
-    # http://identifiers.org/ensembl/ENSG00000178573
-    if target_id:
-        if 'ensembl' in target_id:
-            # ensembl_id = ensemblMatch.groups()[0].rstrip("\s")
-            ensembl_id = target_id.split('/')[-1]
-            if not ensembl_id in self.lookup_data.available_genes:
-                gene_failed = True
-                explanation['unknown_ensembl_gene'] = ensembl_id
-            elif ensembl_id in self.lookup_data.non_reference_genes:
-                explanation['nonref_ensembl_gene'] = ensembl_id
-                gene_mapping_failed = True
-
-        elif 'uniprot' in target_id:
-            uniprot_id =  target_id.split('/')[-1]
-            if uniprot_id not in self.lookup_data.uni2ens:
-                gene_failed = True
-                explanation['unknown_uniprot_entry'] = uniprot_id
-            elif not self.lookup_data.uni2ens[uniprot_id]:#TODO:this will not happen wit the current gene processing pipeline
-                gene_mapping_failed = True
-                gene_failed = True
-                explanation['missing_ensembl_xref_for_uniprot_entry'] = uniprot_id
-
-            elif (uniprot_id in self.lookup_data.uni2ens) and  \
-                            self.lookup_data.uni2ens[uniprot_id] in self.lookup_data.available_genes  and \
-                            'is_reference' in  self.lookup_data.available_genes[self.lookup_data.uni2ens[uniprot_id]]  and \
-                            (not self.lookup_data.available_genes[self.lookup_data.uni2ens[uniprot_id]]['is_reference'] is True) :
-                gene_failed = True
-                explanation['nonref_ensembl_xref_for_uniprot_entry'] = uniprot_id
-            else:
-                try:
-                    reference_target_list = self.lookup_data.available_genes[self.lookup_data.uni2ens[uniprot_id]]['is_reference'] is True
-                except KeyError:
-                    reference_target_list = []
-                if reference_target_list:
-                    target_id = 'http://identifiers.org/ensembl/%s' % reference_target_list[0]
-                else:
-                    # get the first one, needs a better way
-                    target_id = self.lookup_data.uni2ens[uniprot_id]
-                # self.logger.info("Found target id being: %s for %s" %(target_id, uniprot_id))
-                if target_id is None:
-                    self.log_acc.log(l.INFO, "Found no target id for %s", uniprot_id)
-
-
-    # If there is no target id after the processing step
-    if target_id is None:
-        explanation['missing_target_id'] = True
-        gene_failed = True
-
-    # flag as valid or not
-    if not (disease_failed or gene_failed or other_failures):
-        is_valid = True
-    else:
-        explanation['disease_error'] = disease_failed
-        explanation['gene_error'] = gene_failed
-        explanation['gene_mapping_failed'] = gene_mapping_failed
-        self.log_acc.log(l.ERROR, 'evidence validation step failed at the end with an '
-                    'explanation %s', str(explanation))
-
-
 def write_evidences_on_start():
+    '''construct the processcontext to write lines to the files. we have to sets,
+    the good validated ones and the failed ones.
+    '''
     pc = ProcessContext()
     pc.logger.debug("called output_stream from %s", str(os.getpid()))
 
@@ -274,11 +311,16 @@ def process_evidences(filenames, first_n=0, es_client=None, redis_client=None):
                                       autoload=True,
                                       ).lookup
 
+    logger.info('declare pipeline to run')
+    validate_evidence_on_start_f = functools.partial(validate_evidence_on_start, lookup_data)
+
+    # here the pipeline definition
     pl_stage = pr.map(validate_evidence, evs, workers=cpu_count(), maxsize=100,
-                      on_start=validate_evidence_on_start, on_done=validate_evidence_on_done)
+                      on_start=validate_evidence_on_start_f, on_done=validate_evidence_on_done)
     pl_stage = pr.map(write_evidences, pl_stage, workers=2, maxsize=1000, on_start=write_evidences_on_start,
                       on_done=write_evidences_on_done)
 
+    logger.info('run validation pipeline')
     results = reduce_tuple_with_sum(pr.to_iterable(pl_stage))
     logger.info('done validation with %d failed and %d validated', results[0], results[1])
 
