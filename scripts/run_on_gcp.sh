ab5ac636476e3c0e863bb454d23ca53a80e7d07f
Adam Faulconbridge <afaulconbridge@googlemail.com>
2018-12-18 16:36:47 +0000

    update versions and queue size

--- scripts/run_on_gcp.sh
+++ scripts/run_on_gcp.sh
@@ -7,21 +7,25 @@ shopt -s failglob # empty globs are errors
 
 NOW=`date +'%y%m%d-%k%M%S'`
 #underscores are not allowed
-NAME=data-pipeline-$NOW
+NAME=data-pipeline-18-12-3-$NOW
 
 #create an instance
 #  n1-standard-8 is 8 vCPUs and 30GB memory
 #  n1-standard-16 is 16 vCPUs and 60GB memory
+#  n1-standard-32 is 32 vCPUs and 120GB memory
 #  n1-highcpu-16 is 16 vCPUs and 14.4GB memory
 #  n1-highcpu-32 is 32 vCPUs and 28.8GB memory
 #  n1-highcpu-64 is 64 vCPUs and 57.6GB memory
+#  n1-highmem-8 is 8 vCPUs and 52GB memory
+#  n1-highmem-16 is 16 vCPUs and 104GB memory
+#  n1-highmem-32 is 32 vCPUs and 208GB memory
 #Note: for 18.12 release more than 30GB memory is required
 
 #TODO check if it exists already and abort
 gcloud compute instances create $NAME \
   --image-project debian-cloud \
   --image-family debian-9 \
-  --machine-type n1-standard-16 \
+  --machine-type n1-highmem-16 \
   --boot-disk-size 250 \
   --boot-disk-type pd-ssd --boot-disk-device-name $NAME
   
@@ -56,7 +60,9 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 
 #elasticsearch needs a large number of concurrent files
-sudo sysctl -w vm.max_map_count=262144
+sudo echo 'vm.max_map_count=262144' >> /etc/sysctl.conf
+sudo sysctl -p
+echo "vm.max_map_count is `sudo sysctl vm.max_map_count`"
 sudo echo '* hard nofile 10000' >> /etc/security/limits.conf
 sudo echo '* hard memlock infinity' >> /etc/security/limits.conf
 sudo echo /etc/security/limits.conf
@@ -78,14 +84,18 @@ services:
     environment:
       # assign more memory to JVM 
       - "ES_JAVA_OPTS=-Xms8g -Xmx8g"
-      - "xpack.security.enabled=false"
-      - "bootstrap.memory_lock=true"
+      - xpack.security.enabled=false
+      #disable memory swapping to disk for performance
+      - bootstrap.memory_lock=true
+      #increase the amount memory used for indexing write buffers
+      - indices.memory.index_buffer_size=4g
       #allow downloading from google buckets
       - repositories.url.allowed_urls=https://storage.googleapis.com/*,https://*.amazonaws.com/*
     volumes:
       #use a volume for persistence / performance
       - esdata:/usr/share/elasticsearch/data
     ulimits:
+      #disable memory swapping to disk for performance
       memlock:
         soft: -1
         hard: -1
@@ -110,7 +120,7 @@ services:
       - 6379:6379
     
   mrtarget:
-    image: quay.io/opentargets/mrtarget:18.12.3
+    image: quay.io/opentargets/mrtarget:18.12.4
     depends_on:
       - elasticsearch
       - redis
@@ -120,11 +130,46 @@ services:
       - CTTV_REDIS_SERVER=redis:6379
       - SCHEMA_VERSION=1.3.0
       - NUMBER_TO_KEEP=100000000
+      - ES_PREFIX=18.12
+      - "MRTARGET_ARGS=--max-queued-events=16000"
     volumes:
       - ./log:/usr/src/app/log
       - ./json:/usr/src/app/json
       - ./qc:/usr/src/app/qc
 
+  ot_api:
+    image: quay.io/opentargets/rest_api
+    ports:
+      - 8080:80
+    depends_on:
+      - elasticsearch
+    environment:
+      - ELASTICSEARCH_URL=http://elasticsearch:9200
+      - OPENTARGETS_DATA_VERSION=18.12
+    healthcheck:
+      test: ["CMD", "curl", "http://localhost:80/v3/platform/public/utils/stats"]
+      interval: 30s
+      timeout: 500s
+      retries:  30
+
+  ot_webapp:
+    image: quay.io/opentargets/webapp
+    ports:
+      - 8081:80
+      - 7443:443
+    depends_on:
+      - ot_api
+    environment:
+      - REST_API_SCHEME=http
+      - REST_API_SERVER=ot_api
+      - REST_API_PORT=80
+      - APIHOST=localhost
+    healthcheck:
+      test: ["CMD", "curl", "https://localhost:80/api/v3/platform/public/utils/stats"]
+      interval: 30s
+      timeout: 500s
+      retries:  30
+      
 volumes:
   esdata:
     driver: local
@@ -142,7 +187,11 @@ gcloud compute ssh $NAME --command 'sudo docker-compose run --rm --entrypoint ma
     
 #full build!
 #metrics is broken, so dont do it
-gcloud compute ssh $NAME --command 'sudo docker-compose run -d --rm --entrypoint make mrtarget -r -R relationship_data search_data association_qc'
+gcloud compute ssh $NAME --command "cat > run.sh" <<EOF
+docker-compose run -d --rm --entrypoint make mrtarget -r -R -j -l 16 relationship_data search_data association_qc
+EOF
+gcloud compute ssh $NAME --command "chmod +x run.sh"
+gcloud compute ssh $NAME --command 'sudo ./run.sh'
 
 
 #delete the instance at the end
