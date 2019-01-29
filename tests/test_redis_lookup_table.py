7a4e8e3e6e76c1617837169811943daab0fc4d46
Adam Faulconbridge <afaulconbridge@googlemail.com>
2019-01-29 16:21:43 +0000

    fix tests

--- tests/test_redis_lookup_table.py
+++ tests/test_redis_lookup_table.py
@@ -12,7 +12,7 @@ class PicklableObject(object):
 class LookupTableTestCase(unittest.TestCase):
 
     def test_base_lookup(self):
-        with RedisManager(False):
+        with RedisManager(False, "localhost", 35000):
             table = RedisLookupTable()
 
             test = 'this is a string test that does not need to be serialized'
@@ -22,7 +22,7 @@ class LookupTableTestCase(unittest.TestCase):
 
 
     def test_json_lookup(self):
-        with RedisManager(False):
+        with RedisManager(False, "localhost", 35000):
             table = RedisLookupTableJson()
 
             test = {'json test':'this is a test object that can be serialized to json'}
@@ -31,7 +31,7 @@ class LookupTableTestCase(unittest.TestCase):
             self.assertEquals(table.get(key), test)
 
     def test_pickle_lookup(self):
-        with RedisManager(False):
+        with RedisManager(False, "localhost", 35000):
             table = RedisLookupTablePickle()
 
             test = PicklableObject()
